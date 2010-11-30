ac13a306f975541f7058831fb485a68632a4c1f3
adamrocker <adamrocker@2832944c-05cc-d679-43ed-93a393498ae4>
2010-11-30 16:21:33 +0000

    初期インポート。

---
+++ trunk/Plasma/obj/local/armeabi/objs/plasma/plasma.o.d
@@ -0,0 +1,107 @@
+/Users/adamrocker/lib/android-ndk-r4b/samples/bitmap-plasma/obj/local/armeabi/objs/plasma/plasma.o: \
+ /Users/adamrocker/lib/android-ndk-r4b/samples/bitmap-plasma/jni/plasma.c \
+ /Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/jni.h \
+ /Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/time.h \
+ /Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ /Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/sys/time.h \
+ /Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/sys/types.h \
+ /Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/stdint.h \
+ /Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ /Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ /Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/linux/posix_types.h \
+ /Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/linux/stddef.h \
+ /Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/linux/compiler.h \
+ /Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/asm/posix_types.h \
+ /Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/asm/types.h \
+ /Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/linux/types.h \
+ /Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/machine/kernel.h \
+ /Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h \
+ /Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/linux/time.h \
+ /Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/asm/siginfo.h \
+ /Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/asm-generic/siginfo.h \
+ /Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/android/log.h \
+ /Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/android/bitmap.h \
+ /Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/stdio.h \
+ /Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/stdlib.h \
+ /Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/string.h \
+ /Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/malloc.h \
+ /Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/alloca.h \
+ /Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/strings.h \
+ /Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/memory.h \
+ /Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/math.h \
+ /Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/limits.h \
+ /Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/sys/limits.h \
+ /Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/linux/limits.h \
+ /Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/machine/internal_types.h \
+ /Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/machine/limits.h
+
+/Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/jni.h:
+
+/Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/time.h:
+
+/Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/sys/time.h:
+
+/Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/sys/types.h:
+
+/Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/stdint.h:
+
+/Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+
+/Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+
+/Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/linux/stddef.h:
+
+/Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/linux/compiler.h:
+
+/Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/asm/types.h:
+
+/Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/linux/types.h:
+
+/Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/machine/kernel.h:
+
+/Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h:
+
+/Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/linux/time.h:
+
+/Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/asm/siginfo.h:
+
+/Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/asm-generic/siginfo.h:
+
+/Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/android/log.h:
+
+/Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/android/bitmap.h:
+
+/Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/stdio.h:
+
+/Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/stdlib.h:
+
+/Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/string.h:
+
+/Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/malloc.h:
+
+/Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/alloca.h:
+
+/Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/strings.h:
+
+/Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/memory.h:
+
+/Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/math.h:
+
+/Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/limits.h:
+
+/Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/sys/limits.h:
+
+/Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/linux/limits.h:
+
+/Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/machine/internal_types.h:
+
+/Users/adamrocker/lib/android-ndk/build/platforms/android-8/arch-arm/usr/include/machine/limits.h:
